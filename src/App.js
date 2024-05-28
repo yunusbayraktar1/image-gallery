6f6a8abc3a2591c2d7705db43a248fa11317dfab
YunusBayraktar <123024235+YunusBayraktar@users.noreply.github.com>
2024-05-28 23:39:28 +0300

    simple image gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import images from './images'
+import React , {useState} from 'react';
+import './style.css';
 function App() {
+
+  const [selected,setSelected] = useState("https://cdn.pixabay.com/photo/2023/02/08/18/36/tawny-owl-7777285_640.jpg");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+    <img className='selected' src={selected}/>
+      </div>
+      <div className='imgContainer'>
+    {images.map((img,index)=>(
+      <img src={img} key={index} onClick={()=>setSelected(img)}/>
+    ))}
+      </div>
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,7 @@
+export default[
+"https://cdn.pixabay.com/photo/2023/02/08/18/36/tawny-owl-7777285_640.jpg",
+"https://cdn.pixabay.com/photo/2023/01/03/16/00/dog-7694676_640.jpg",
+"https://cdn.pixabay.com/photo/2024/04/09/15/36/after-the-rain-8686213_640.jpg",
+"https://cdn.pixabay.com/photo/2022/11/29/11/30/lake-7624330_640.jpg",
+"https://cdn.pixabay.com/photo/2023/08/18/15/02/cat-8198720_640.jpg"
+]
